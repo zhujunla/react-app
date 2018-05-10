25a237d5468595c985e68fcb0d11bc1bd3fca3fd
朱骏梁 <770710869@qq.com>
2018-05-10 10:58:55 +0800

    build前删除原来的build文件！

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,6 +8,9 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');//清理打包文件
 
 module.exports =webpackMerge(webpackBase,{
     // context: __dirname + '/build',
+    performance: {//不弹出合并的js超出大小弹出警告错误
+        hints: false
+      },
     plugins: [
          new CleanWebpackPlugin(['build']),
     ]
